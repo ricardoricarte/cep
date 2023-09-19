cdc1df901cf414d32eaaa7aa267c9eb3d5016716
José Ricardo <56279938+ricardoricarte@users.noreply.github.com>
2023-09-19 12:52:13 -0300

    Cep

---
+++ src/main/java/com/cep/CepResultDTO.java
@@ -0,0 +1,96 @@
+package com.cep;
+
+public class CepResultDTO {
+
+  private String cep;
+  private String lougradouro;
+  private String complemento;
+  private String bairro;
+  private String localidade;
+  private String uf;
+  private String ibge;
+  private String gia;
+  private String ddd;
+  private String siafy;
+
+  public String getCep() {
+    return cep;
+  }
+  public void setCep(String cep) {
+    this.cep = cep;
+  }
+  public String getLougradouro() {
+    return lougradouro;
+  }
+  public void setLougradouro(String lougradouro) {
+    this.lougradouro = lougradouro;
+  }
+  public String getComplemento() {
+    return complemento;
+  }
+  public void setComplemento(String complemento) {
+    this.complemento = complemento;
+  }
+  public String getBairro() {
+    return bairro;
+  }
+  public void setBairro(String bairro) {
+    this.bairro = bairro;
+  }
+  public String getLocalidade() {
+    return localidade;
+  }
+  public void setLocalidade(String localidade) {
+    this.localidade = localidade;
+  }
+  public String getUf() {
+    return uf;
+  }
+  public void setUf(String uf) {
+    this.uf = uf;
+  }
+  public String getIbge() {
+    return ibge;
+  }
+  public void setIbge(String ibge) {
+    this.ibge = ibge;
+  }
+  public String getGia() {
+    return gia;
+  }
+  public void setGia(String gia) {
+    this.gia = gia;
+  }
+  public String getDdd() {
+    return ddd;
+  }
+  public void setDdd(String ddd) {
+    this.ddd = ddd;
+  }
+  public String getSiafy() {
+    return siafy;
+  }
+  public void setSiafy(String siafy) {
+    this.siafy = siafy;
+  } 
+
+}
+
+
+
+  /** 
+
+  {
+    "cep": "01001-000",
+    "logradouro": "Praça da Sé",
+    "complemento": "lado ímpar",
+    "bairro": "Sé",
+    "localidade": "São Paulo",
+    "uf": "SP",
+    "ibge": "3550308",
+    "gia": "1004",
+    "ddd": "11",
+    "siafi": "7107"
+  }
+  /* */
+
